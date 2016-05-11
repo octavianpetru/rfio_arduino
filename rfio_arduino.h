1757238547bb1fbc21b482c2f4f1ec58a81ff758
Octavian Petru <octavian.petru@gmail.com>
2016-05-11 17:08:06 +0300

    First commit

---
+++ rfio_arduino.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _rfio_arduino_H_
+#define _rfio_arduino_H_
+#include "Arduino.h"
+//add your includes for the project rfio_arduino here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project rfio_arduino here
+
+
+
+
+//Do not add code below this line
+#endif /* _rfio_arduino_H_ */
